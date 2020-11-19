2d24228d2b8cf790bef7dca2de130836fa51451c
Fabiolalfaro <66134812+Fabiolalfaro@users.noreply.github.com>
2020-11-19 15:24:48 -0600

    Create opcinesDig.php

---
+++ opcinesDig.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    
+    <!-- <link rel="stylesheet" href="css/estilo.css"> -->
+
+    <title>BD2</title>
+  </head>
+  <body><br><center>
+    <h3>Opciones</h3>
+  </center>
+
+    
+    <div class="row" align="center">
+
+        <div class="col-sm-3">
+            <div class="card">
+            <div class="card-header">
+              Partidas
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">
+                
+              </h5>
+              <p class="card-text">
+                
+              </p>
+              <a href="#" class="btn btn-primary">Partida nueva</a>
+            </div>
+          </div>
+</div>
+
+
+        
+    </body>
+</html>
