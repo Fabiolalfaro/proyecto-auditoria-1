68674b39314d2e10625f886e879d5990102c670f
Fabiolalfaro <66134812+Fabiolalfaro@users.noreply.github.com>
2020-11-19 15:29:52 -0600

    Update flogin.php

--- flogin.php
+++ flogin.php
@@ -4,13 +4,15 @@
     $sql=new connection();
     $conexion = $sql-> get_conection();
 
-    $consulta=("call paLogin2(?,?)");
+    $consulta=("call paLogin(?,?)");
     $usuario=($_POST['usu']);
     $pass=($_POST['pass']);
 
 
-    
-    $op=(0);
+    $rol=0;
+    $inicio=0;
+
+    $op=(4);
     $time = time();
     $fecha=(date("d-m-Y (H:i:s)", $time));
 
@@ -28,33 +30,60 @@
     $direc=(getRealIP());
 
 
+
     if($sentencia = $conexion->prepare($consulta)){
 
       $sentencia->bind_param('ss',$usuario,$pass);
 
       $sentencia->execute();
+
       $sentencia->store_result();
       $inicio=$sentencia->num_rows();
-
+      
       if($inicio<=0){
-        echo "Datos Erroneos";
-
+        echo "<script>alert('DATOS INCORRECTOS');history.go(-1)</script>";
         $desc=("Intento de ingreso al sistema");
-
+        $ev=1;
 
 
       }else{
-        echo "Bienvenido al Sistema";
+
+          $sql=new connection();
+          $conexion = $sql-> get_conection();
+          $sentencia = $conexion->prepare($consulta);
+          $sentencia->bind_param('ss',$usuario,$pass);
+          $sentencia ->execute();
+          $sentencia->bind_result($usu,$password,$cod_rol);
+          $sentencia->fetch();
+
+
+         switch ($cod_rol) {
+           case '1':
+             header('Location: opcinesAdmin.php?usu=$usu') ;
+             break;
+
+             case '2':
+             header('Location: opcinesDig.php');
+             break;
+           
+           default:
+             # code...
+             break;
+         }
+
+
+
+      
 
         $desc=("Ingreso al sistema");
-        
+        $ev=2;
       }
 
-      $sql=new connection();
+          $sql=new connection();
           $con = $sql->get_conection();
           $consulta=("call paVit1(?,?,?,?,?,?)");
           $sentencia = $con->prepare($consulta);
-          $sentencia->bind_param('ssssss',$usuario,$op,$op,$direc,$desc,$fecha);
+          $sentencia->bind_param('ssssss',$usuario,$op,$ev,$direc,$desc,$fecha);
           $sentencia->execute();
           $sentencia->close();
     }
